d15637628bb3a4c89bd06146cec298189e9ee980
Bill <yeluyang530@163.com>
2018-09-14 22:03:50 +1000

    Create two pages - Login and Dashboard

---
+++ app/components/Home/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text, TextInput } from 'react-native'
+ 
+class  Home extends Component {
+    render() {
+        return (
+        <View>
+               <Text>Login Demo App</Text>
+               <TextInput />
+               <TextInput />
+        </View>
+        )
+    }
+}
+
+export default Home 
